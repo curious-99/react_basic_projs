d40562a0a28e2e88ec31046e33f835b669fd498a
curious-99 <mohit39.ugcs20@iiitranchi.com>
2023-10-10 19:06:27 +0530

    React-Router Crash

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2 className='bg-green-600 p-4 '> React Router</h2>
+    </>
+  )
+}
+
+export default App
